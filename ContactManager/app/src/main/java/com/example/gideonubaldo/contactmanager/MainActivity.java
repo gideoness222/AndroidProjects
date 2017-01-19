aa8fe6301de2c454eeb32c59a3245c7628294bbb
gideonubaldo <gideoness222@gmail.com>
2017-01-19 10:48:42 -0800

    wip

--- ContactManager/app/src/main/java/com/example/gideonubaldo/contactmanager/MainActivity.java
+++ ContactManager/app/src/main/java/com/example/gideonubaldo/contactmanager/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.gideonubaldo.contactmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nametxt,phonetxt,emailtxt,addresstxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nametxt = (EditText)findViewById(R.id.etName);
+        phonetxt = (EditText)findViewById(R.id.etPhoneNumber);
+        emailtxt = (EditText)findViewById(R.id.etEmail);
+        addresstxt = (EditText)findViewById(R.id.etAddress);
+        final Button addBtn = (Button)findViewById(R.id.addButton);
+
+        nametxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addBtn.setEnabled(!nametxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
